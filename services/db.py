34bacff8a550c827324f3d9e22c896e799315f51
RamEklare <rameklare75@gmail.com>
2025-09-03 22:50:05 +0530

    Add files via upload

--- services/db.py
+++ services/db.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-def load_patients(path="C:/Users/HP/medical_scheduling_agent/patients_sample_50.csv"):
+def load_patients(path="C:/Users/HP/medical_scheduling_agent/data/patients_sample_50.csv"):
     """Load patient data from your CSV."""
     return pd.read_csv(path)
